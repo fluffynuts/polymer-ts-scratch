d9fc48a86273bc3760aab7d8915c20da1cf1bb08
Brendon Page <brendon.page@chillisoft.co.za>
2017-03-02 10:28:46 +0200

    feat: got rid of custom logic for displaying pages, used iron-lazy-pages instead

--- src/app/sts-routes/element.ts
+++ src/app/sts-routes/element.ts
@@ -1,11 +1,5 @@
 @component('sts-routes')
 class StsRoutes extends polymer.Base {
-  constructor() {
-    super();
-  }
-  _equals(val1, val2): boolean {
-    return val1 === val2;
-  }
 }
 
 StsRoutes.register();
